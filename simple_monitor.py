61abc0e19229c191e6fcf8ebcbdf28dccd06cd15
satrianachandra <satrianachandra@gmail.com>
2015-03-04 20:34:19 -0300

    add disable port

--- simple_monitor.py
+++ simple_monitor.py
@@ -6,6 +6,9 @@ from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.lib import hub
 
+#import urllib
+#import urllib2
+import requests
 
 class SimpleMonitor(simple_switch_13.SimpleSwitch13):
 
@@ -16,6 +19,7 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
         self.rx_bytes = [0,0]
         self.tx_bytes = [0,0]
         self.utilization= [0.0,0.0]
+        self.ports = [1,1]
         self.monitoring_time = 5;
 
     @set_ev_cls(ofp_event.EventOFPStateChange,
@@ -71,3 +75,78 @@ class SimpleMonitor(simple_switch_13.SimpleSwitch13):
                 self.logger.info('\nutilization port 1: %f', self.utilization[stat.port_no-1] )
                 self.tx_bytes [stat.port_no - 1] = stat.tx_bytes
                 self.rx_bytes [stat.port_no - 1] = stat.rx_bytes
+                
+
+                self.logger.info("\nrerouting\n")
+                self.reroute(self.mac_to_port)
+
+                '''
+                if ( (port2 is down) and port1_util > 70 ) :
+                    #enable port2
+                    self._modify_port(2,0)
+                    self.ports[1]=1
+                    self.reroute()
+                else if ( (port2 is up) and port1_util + port2_util < 90) :
+                    self.reroute()
+                    #disable port2
+                    self._modify_port(2,8)
+                    self.ports[1]=0
+                '''
+
+
+    def _modify_port(self,port_no,config):
+        url = 'http://localhost:8080/stats/portdesc/modify'
+        payload = {
+          'dpid': 1,
+          'port_no': port_no,
+          'config': config,
+          'mask' : 0
+        }
+        response = requests.post(url,params=payload)
+        self.logger.info(response.text)
+        
+
+
+    def reroute(self,mac_to_port):
+
+        hit = 0
+        mac_h1 = ""
+        for mac, port in mac_to_port[1].items():
+            if port == 1:
+                hit = hit + 1
+                if hit==1:
+                    mac_h1 = mac
+                    self.logger.info(mac_h1)
+                    hit=0
+                    break
+
+
+        #if port1 and port2 is active:
+            #modify the flows
+            #set so that traffic from odd ports come to port 1, and from even ports come to port 2
+        url = 'http://localhost:8080/stats/flowentry/modify'
+        port_output =  1
+        for i in range(1,5):
+
+            if i % 2 == 0:
+                port_output = 2
+            else:
+                port_output = 1
+
+            payload = {
+              "dpid": 1,
+              "match":{
+                     "dl_dst": mac_h1,
+                     "in_port": i
+                },
+              "actions":[
+                    {
+                        "type":"OUTPUT",
+                        "port": port_output
+                    }
+              ]
+            }
+            
+            response = requests.post(url,params=payload)
+            self.logger.info(response.text)
+
